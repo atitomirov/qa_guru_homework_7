48b4475bdfc4bfdf445196abf56cd82ad88b8331
atitomirov <burjui777@yandex.ru>
2023-06-08 22:58:34 +0300

    added remote test

---
+++ src/test/java/com/demoqa/tests/RegistrationRemoteTests.java
@@ -0,0 +1,44 @@
+package com.demoqa.tests;
+
+import com.demoqa.pages.RegistrationPage;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+
+import static com.demoqa.tests.TestData.*;
+
+public class RegistrationRemoteTests extends RemoteTestBase {
+    RegistrationPage registrationPage = new RegistrationPage();
+
+    @Test
+    @Tag("remote")
+    void successfulRegistrationTest() {
+
+        registrationPage.openPage()
+                .removeBanner()
+                .setFirstName(firstName)
+                .setLastName(lastName)
+                .setUserEmail(userEmail)
+                .setGender(gender)
+                .setNumber(mobile)
+                .setBirthDate(day, month, year)
+                .setSubjects(subject)
+                .setHobbies(hobbies)
+                .setPicture(imageUploadPath)
+                .setAddress(currentAddress)
+                .setState(state)
+                .setCity(city)
+                .setSubmit();
+
+        registrationPage.verifyRegistrationResultsModalAppears()
+                .verifyResult("Student Name", firstName + " " + lastName)
+                .verifyResult("Student Email", userEmail)
+                .verifyResult("Gender", gender)
+                .verifyResult("Mobile", mobile)
+                .verifyResult("Date of Birth", String.format("%s %s,%s", day, month, year))
+                .verifyResult("Subjects", subject)
+                .verifyResult("Hobbies", hobbies)
+                .verifyResult("Picture", imageName)
+                .verifyResult("Address", currentAddress)
+                .verifyResult("State and City", state + " " + city);
+    }
+}
