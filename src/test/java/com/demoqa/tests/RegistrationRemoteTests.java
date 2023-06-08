de0843096434f07c872a6d033c68f115c13be1a4
atitomirov <burjui777@yandex.ru>
2023-06-09 00:23:09 +0300

    added attach

--- src/test/java/com/demoqa/tests/RegistrationRemoteTests.java
+++ src/test/java/com/demoqa/tests/RegistrationRemoteTests.java
@@ -1,44 +1,53 @@
 package com.demoqa.tests;
 
 import com.demoqa.pages.RegistrationPage;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
 import static com.demoqa.tests.TestData.*;
+import static io.qameta.allure.Allure.step;
 
 public class RegistrationRemoteTests extends RemoteTestBase {
     RegistrationPage registrationPage = new RegistrationPage();
 
     @Test
     @Tag("remote")
+    @DisplayName("Successful registration")
     void successfulRegistrationTest() {
 
-        registrationPage.openPage()
-                .removeBanner()
-                .setFirstName(firstName)
-                .setLastName(lastName)
-                .setUserEmail(userEmail)
-                .setGender(gender)
-                .setNumber(mobile)
-                .setBirthDate(day, month, year)
-                .setSubjects(subject)
-                .setHobbies(hobbies)
-                .setPicture(imageUploadPath)
-                .setAddress(currentAddress)
-                .setState(state)
-                .setCity(city)
-                .setSubmit();
+        step("Open form", () -> {
+            registrationPage.openPage()
+                    .removeBanner();
+        });
 
-        registrationPage.verifyRegistrationResultsModalAppears()
-                .verifyResult("Student Name", firstName + " " + lastName)
-                .verifyResult("Student Email", userEmail)
-                .verifyResult("Gender", gender)
-                .verifyResult("Mobile", mobile)
-                .verifyResult("Date of Birth", String.format("%s %s,%s", day, month, year))
-                .verifyResult("Subjects", subject)
-                .verifyResult("Hobbies", hobbies)
-                .verifyResult("Picture", imageName)
-                .verifyResult("Address", currentAddress)
-                .verifyResult("State and City", state + " " + city);
+        step("Fill form", () -> {
+            registrationPage.setFirstName(firstName)
+                    .setLastName(lastName)
+                    .setUserEmail(userEmail)
+                    .setGender(gender)
+                    .setNumber(mobile)
+                    .setBirthDate(day, month, year)
+                    .setSubjects(subject)
+                    .setHobbies(hobbies)
+                    .setPicture(imageUploadPath)
+                    .setAddress(currentAddress)
+                    .setState(state)
+                    .setCity(city)
+                    .setSubmit();
+        });
+        step("Verify results", () -> {
+            registrationPage.verifyRegistrationResultsModalAppears()
+                    .verifyResult("Student Name", firstName + " " + lastName)
+                    .verifyResult("Student Email", userEmail)
+                    .verifyResult("Gender", gender)
+                    .verifyResult("Mobile", mobile)
+                    .verifyResult("Date of Birth", String.format("%s %s,%s", day, month, year))
+                    .verifyResult("Subjects", subject)
+                    .verifyResult("Hobbies", hobbies)
+                    .verifyResult("Picture", imageName)
+                    .verifyResult("Address", currentAddress)
+                    .verifyResult("State and City", state + " " + city);
+        });
     }
 }
